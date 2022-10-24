a45bc103e8457189d5958ec6db99487742c9b26b
Sergio Pérez <sergio-luis98@hotmail.com>
2022-10-24 12:09:01 -0500

    superuser

--- build.sh
+++ build.sh
@@ -7,3 +7,4 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
+python manage.py createsuperuser --username admin --email mail@mail.com  --noinput
